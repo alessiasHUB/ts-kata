d90367e78bebc4fa89f514891b539b60a1a67351
Alessia B <borys.alessia@gmail.com>
2022-10-27 11:03:26 +0100

    x marks the spot

---
+++ src/7kyu/x-marks-the-spot.test.ts
@@ -0,0 +1,53 @@
+import xMarksTheSpot from "./x-marks-the-spot";
+
+test("Returns coordinates with only one x", () => {
+  expect(
+    xMarksTheSpot([
+      ["o", "o", "o", "o", "o", "o", "o", "o"],
+      ["o", "o", "o", "o", "o", "o", "o", "o"],
+      ["o", "o", "o", "o", "o", "o", "o", "o"],
+      ["o", "o", "o", "o", "o", "o", "o", "o"],
+      ["o", "o", "o", "o", "o", "o", "x", "o"],
+      ["o", "o", "o", "o", "o", "o", "o", "o"],
+    ])
+  ).toStrictEqual([4, 6]);
+});
+
+test("Returns empty array when no X's present", () => {
+    expect(
+      xMarksTheSpot([
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+      ])
+    ).toStrictEqual([]);
+  });
+
+  test("Returns empty array when multiple X's present", () => {
+    expect(
+      xMarksTheSpot([
+        ["x", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "x"],
+      ])
+    ).toStrictEqual([]);
+  });
+
+  test("Returns empty array when multiple X's on same line present", () => {
+    expect(
+      xMarksTheSpot([
+        ["x", "x", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o", "o", "o", "o"],
+      ])
+    ).toStrictEqual([]);
+  });
---
+++ src/7kyu/x-marks-the-spot.ts
@@ -0,0 +1,39 @@
+/**
+ * codewars link: https://www.codewars.com/kata/5777fe3f355edbf0a5000d11
+ * =======================================================================
+ * Task:
+ * Given a two dimensional array, return the co-ordinates of x.
+ * If x is not inside the array, or if x appears multiple times, return [].
+ * The co-ordinates should be zero indexed in row-major order.
+ * You should assume you will always get an array as input. 
+ * The array will only contain 'x's and 'o's.
+ * ========================================================================
+ * @params input - [][]
+ * @returns number[]
+ * 
+ * PROGRAMME xMarksTheSpot(input):
+ * Set COORDINATES to equal []
+ * FOR EACH array in input:
+ *      FOR EACH element in the array:
+ *         IF element is equal to 'x'
+ *             Set COORDINATES to location of 'x'
+ *         END IF.
+ *     END FOR.
+ * RETURN COORDINATES
+ */
+
+ function xMarksTheSpot(input: string[][]): number[]{
+    const coordinates = [];
+    for (let i = 0; i < input.length; i++) {
+      for (let j = 0; j < input[i].length; j++) {
+        if (input[i][j] === "x") {
+          coordinates.push(i);
+          coordinates.push(j);
+        }
+      }
+    }
+    return coordinates.length === 2 ? coordinates : [];
+  }
+  
+  export default xMarksTheSpot;
+  
